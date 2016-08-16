5c19a34e72eba2b7b7cd9026fbdb637e6f9fcc21
Segun Famisa <segunfamisa@gmail.com>
2016-08-16 22:56:21 +0100

    chore: Modified sample activity to demonstrate usage

--- sample/src/main/java/com/segunfamisa/icicle/sample/MainActivity.java
+++ sample/src/main/java/com/segunfamisa/icicle/sample/MainActivity.java
@@ -1,24 +1,42 @@
 package com.segunfamisa.icicle.sample;
 
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.segunfamisa.icicle.Icicle;
+import com.segunfamisa.icicle.annotations.Freeze;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Freeze String name;
+    @Freeze String age;
+    @Freeze int houses;
+    @Freeze HashMap<String, String> map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Icicle.thaw(this, savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            map = new HashMap<>();
+            map.put("A", "Apple");
+            name = "Segun";
+            age = "23";
+            houses = 5;
+        }
+
+        Log.d("MainActivity", map.toString() + "\n" + name + "\n" + age + "\n" + houses);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        Icicle.freeze(this, outState);
         super.onSaveInstanceState(outState);
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-    }
 }
